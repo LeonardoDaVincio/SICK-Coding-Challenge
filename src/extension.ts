c0ffc765c91806d85ff426010a7ff4a81cfdd9a0
Lennard Sommer <lennard.sommer@gmx.de>
2022-12-11 12:36:44 +0100

    Passt vscodeignore an um alle node_modules zu ignorieren, passt den Command Name an

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,10 @@ import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 
 export function activate(context: ExtensionContext) {
   // Create the show hello world command
-  const showHelloWorldCommand = commands.registerCommand("firmware-compatibility-matcher.helloWorld", () => {
+  const startFWCM = commands.registerCommand("firmware-compatibility-matcher.startFWCM", () => {
     HelloWorldPanel.render(context.extensionUri);
   });
 
   // Add command to the extension context
-  context.subscriptions.push(showHelloWorldCommand);
+  context.subscriptions.push(startFWCM);
 }
